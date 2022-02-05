52254b0e0ae8c158bb5ce3c81b3e1daa2ac4fd08
pushpak pantawane <pushpakpantawane19@gmail.com>
2022-02-05 22:49:19 +0530

    updated

---
+++ web development/Untitled-1.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+// int a=3;
+// long long b=12345678912345;
+// char c='a';
+// float x=334.23;
+// long double y=14049.30493;
+// // cin>>a;
+// // cin>>b;
+// // cin>>c;
+// // cin>>x;
+// // cin>>y;
+// // cout<<a<<endl<<b<<endl<<c<<endl<<x<<endl<<y<<endl;
+// cout<<a<<endl;
+// cout<<b<<endl;
+// cout<<c<<endl;
+// cout<<x<<endl;
+// cout<<y<<endl;
+
+// int i;
+// cout<<"enter i";
+// cin>>i;
+// for ( i = 0; i <9; i++)
+// {
+// if (1==1)
+// {
+//     cout<<"one";
+//     i++;
+//     // break;
+// }
+// else if(2==2){cout<<"two";
+// i++;
+// // break;
+// }
+// else if (3==3){cout<<"three";
+// break;}
+// else{cout<<"greater";}
+
+// }
+int a,b;
+cin>>a>>b;
+int sum;
+sum=a+b;
+cout<<sum;
+
+
+    return 0;
+}
